7b2d74de660db867d3a248e98c64ddbb45ab6e2d
DennerLuan <denner.luan@prox3.com.br>
2013-05-20 23:17:07 -0300

    Página de clientes

---
+++ clientes.php
@@ -0,0 +1,39 @@
+<?php $title = 'MegaGeo';
+	  $description = 'Com equipamentos modernos aliados a um com corpo técnico especializado, a MegaGeo está altamente capacitada para trabalhos de fundação.';
+	  $keywords = 'engenharia, fundacoes, bate estaca, obras';
+	  $organization = 'MegaGeo';
+ ?>
+<?php require("header.php")?>
+<style type="text/css">
+	#banner-obras{width:897px; height:146px; border:solid 4px #000; margin-bottom:28px}
+	.title_page{width:100%; height:19px; float:left; font-size:20px; color:#e77843; letter-spacing:1px; margin-bottom:17px}
+	.arrow_page{width: 0; height: 0; border-right-width: 7px; border-top: 7px solid transparent; border-left: 7px solid transparent; border-bottom-width: 7px; float: left;margin-right: 5px; margin-top: 4px; border-right-color: #e77843; border-style: solid; border-bottom-color: #e77843;}
+	.box_clients{padding:22px 0 22px 27px; margin:0; float:left; background:#d4d4d4; width:400px; height:134px; list-style-type:circle !important; list-style:circle; margin-bottom:26px}
+	.box_clients li{font-size:12px; color:#707070; width:100%; float: left; list-style:circle; margin-bottom:20px; list-style-type:circle !important; list-style:circle}
+	.margin-0{margin:0 !important}
+</style>
+<div id="content-home">
+	<div class="content-home-absolute" style="background:#e8e8e8"></div>
+    <div id="banner-obras">
+    	<img src="images/megageo-obras.jpg" width="897" height="146" alt="" title="" />
+    </div>
+    <div class="title_page">
+    	<font class="arrow_page"></font>CLIENTES
+    </div>
+    <ul class="box_clients">
+    	<li>&bull; ENAC Empreendimentos, Construção e Comércio Ltda.</li>
+      <li>&bull; Construtora Guarany</li>
+      <li>&bull; Queiroz Galvão Desenvolvimento Imobiliário</li>
+      <li>&bull; Rossi Residencial AS</li>
+      <li class="margin-0">&bull; HS Tressoldi Incorporações Ltda</li>
+    </ul>
+    
+    <ul class="box_clients" style="float:right">
+    	<li>&bull; Construcamp obras e projetos Ltda</li>
+      <li>&bull; Concima Construtora</li>
+      <li>&bull; HM Engenharia</li>
+      <li>&bull; J.Fonseca Construtora</li>
+      <li class="margin-0">&bull; Construtora Nogueira Porto</li>
+    </ul>
+</div>
+<?php require("footer.php")?>
