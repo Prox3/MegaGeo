907f566660282f12e7ef52c8baf7fa2d25a2cda3
DennerLuan <denner.luan@prox3.com.br>
2013-05-20 22:36:28 -0300

    Página de contato

---
+++ obras.php
@@ -0,0 +1,81 @@
+<?php $title = 'MegaGeo';
+	  $description = 'Com equipamentos modernos aliados a um com corpo técnico especializado, a MegaGeo está altamente capacitada para trabalhos de fundação.';
+	  $keywords = 'engenharia, fundacoes, bate estaca, obras';
+	  $organization = 'MegaGeo';
+ ?>
+<?php require("header.php")?>
+<style type="text/css">
+	#banner-obras{width:897px; height:146px; border:solid 4px #000; margin-bottom:28px}
+	.title_page{width:100%; height:19px; float:left; font-size:20px; color:#e77843; letter-spacing:1px; margin-bottom:17px}
+	.arrow_page{width: 0; height: 0; border-right-width: 7px; border-top: 7px solid transparent; border-left: 7px solid transparent; border-bottom-width: 7px; float: left;margin-right: 5px; margin-top: 4px; border-right-color: #e77843; border-style: solid; border-bottom-color: #e77843;}
+	.box_obras{width:272px; height:96px; background:#d4d2d2; padding:5px; float:left; margin:0 0 26px 29px}
+	.box_obras img{width:101px; height:92px; border:solid 2px #000; float:left; display:block}
+	.box_title_obras{float:left; padding:5px 20px 4px 7px; background:#bababa; text-transform:uppercase; font-size:12px; margin:5px 0 6px 5px}
+	.box_local_obras{width:160px; height:auto; font-size:11px; margin:0 0 6px 5px; float:left}
+	.caracteristicas_obras{text-align:justify}
+	.margin-0{margin:0}
+</style>
+<div id="content-home">
+	<div class="content-home-absolute" style="background:#e8e8e8"></div>
+    <div id="banner-obras">
+    	<img src="images/megageo-obras.jpg" width="897" height="146" alt="" title="" />
+    </div>
+    <div class="title_page">
+    	<font class="arrow_page"></font>OBRAS
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Nome da Obra</div>
+      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      </div>
+    </div>
+		
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Nome da Obra</div>
+      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Nome da Obra</div>
+      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Nome da Obra</div>
+      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      </div>
+    </div>
+		
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Nome da Obra</div>
+      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Nome da Obra</div>
+      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      </div>
+    </div>
+</div>
+<?php require("footer.php")?>
