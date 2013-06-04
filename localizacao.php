baa11f4b36f36183e4e3330e2619d14ee9521de7
DennerLuan <denner.luan@prox3.com.br>
2013-06-03 21:14:50 -0300

    localizacao

---
+++ localizacao.php
@@ -0,0 +1,22 @@
+<?php $activeMenu = '#orcamentos'; ?>
+<style type="text/css">
+	#body_map{width:650px; height:405px; float:left; border:solid 18px #e3e2e2}
+	#body_text_local{width:164px; height:auto; float:left; background:url(images/point_local.png) no-repeat; margin:18px 0 0 16px; padding-left:31px; text-align:justify; font-size:12px; color:#777777}
+	#body_tel{width:164px; height:auto; float:left; background:url(images/ico_tel.png) no-repeat; margin:18px 0 0 16px; padding-left:31px; text-align:justify; font-size:12px; color:#777777}
+</style>
+<!-- Inicio Section 6 Contato  -->
+<div id="struct-orcamento">
+	<div id="titulo-orcamento">
+        <div class="arrows-titulo"></div>
+        <div class="titulo-h3">
+            <h3>Localização</h3>
+        </div>
+    </div>
+    
+		<div id="body_map">
+    	<iframe width="650" height="406" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.br/maps?q=Rua+Anita+Moretzshon,+243+SL+10+Campinas,+SP+13088-603&amp;ie=UTF8&amp;hq=&amp;hnear=R.+Anita+Moretzshon,+243+-+Jardim+Santana,+Campinas+-+S%C3%A3o+Paulo,+13088-603&amp;gl=br&amp;t=m&amp;z=14&amp;ll=-22.853057,-47.042513&amp;output=embed"></iframe>
+    </div>
+    <div id="body_text_local">Rua Anita Moretzshon, 243 SL 10<br />Campinas, SP<br />13088-603</div>
+    <div id="body_tel">+55 19 9999-9999<br />+55 19 9999-9999</div>
+</div>
+<!-- Fim Section 6 Contato  -->
