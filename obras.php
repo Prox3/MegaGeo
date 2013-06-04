166d08954826e78829d71273542ccb9390d41386
DennerLuan <denner.luan@prox3.com.br>
2013-06-04 01:01:31 -0300

    equipamentos

--- obras.php
+++ obras.php
@@ -10,55 +10,279 @@
     
     <div class="box_obras margin-0">
     	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
-      <div class="box_title_obras">Nome da Obra</div>
-      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_title_obras">VIVO</div>
+      <div class="box_local_obras"><strong>Local: </strong>Santana do Parnaíba/SP</div>
       <div class="box_local_obras caracteristicas_obras">
-      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      	<strong>Cliente: </strong>Racional Engenharia Ltda
       </div>
     </div>
 		
     <div class="box_obras">
     	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
-      <div class="box_title_obras">Nome da Obra</div>
-      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_title_obras">Residencial Lumiere</div>
+      <div class="box_local_obras"><strong>Local: </strong>Piracicaba/SP</div>
       <div class="box_local_obras caracteristicas_obras">
-      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      	<strong>Cliente: </strong>Construtora Garopaba
       </div>
     </div>
     
     <div class="box_obras">
     	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
-      <div class="box_title_obras">Nome da Obra</div>
-      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_title_obras">Edifício Letóile</div>
+      <div class="box_local_obras"><strong>Local: </strong>Campinas/SP</div>
       <div class="box_local_obras caracteristicas_obras">
-      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      	<strong>Cliente: </strong>Construtora Valadares Contijo S/A
       </div>
     </div>
     
     <div class="box_obras margin-0">
     	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
-      <div class="box_title_obras">Nome da Obra</div>
-      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_title_obras">Prédio Resid. (03 Torres)</div>
+      <div class="box_local_obras"><strong>Local: </strong>Valinhos/SP</div>
       <div class="box_local_obras caracteristicas_obras">
-      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      	<strong>Cliente: </strong>Cooperativa Habitacional Nova Aliança
       </div>
     </div>
 		
     <div class="box_obras">
     	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
-      <div class="box_title_obras">Nome da Obra</div>
-      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_title_obras">Ed. Clermont Ferrant</div>
+      <div class="box_local_obras"><strong>Local: </strong>Campinas/SP</div>
       <div class="box_local_obras caracteristicas_obras">
-      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      	<strong>Cliente: </strong>CPN Engenharia e Construções Ltda
       </div>
     </div>
     
     <div class="box_obras">
     	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
-      <div class="box_title_obras">Nome da Obra</div>
-      <div class="box_local_obras"><strong>Local: </strong>Local da obra</div>
+      <div class="box_title_obras">Galpão Industrial</div>
+      <div class="box_local_obras"><strong>Local: </strong>Jundiaí/SP</div>
       <div class="box_local_obras caracteristicas_obras">
-      	<strong>Características: </strong>Informações sobre a obra Informações sobre a obra Informações sobre a obra
+      	<strong>Cliente: </strong>PP Snacks Serviços Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Galpão Industrial</div>
+      <div class="box_local_obras"><strong>Local: </strong>Jundiaí/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Cilare Empreendimentos
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Prédio Residencial</div>
+      <div class="box_local_obras"><strong>Local: </strong>São José dos Campos/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Planej Construtora e Incorporadora Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Concessionária T. Ford</div>
+      <div class="box_local_obras"><strong>Local: </strong>Concessionária Tempo Ford</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Enac Empreendimento e Construções
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Condomínios Residenciais</div>
+      <div class="box_local_obras"><strong>Local: </strong>Cajamar/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>HM 17 Empreendimentos Imobiliários SPE
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">HM25 – Pq. das Colinas</div>
+      <div class="box_local_obras"><strong>Local: </strong>Valinhos/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>HM 25 Empreendimentos Imobiliários SPE
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">S. Irmãos Russi</div>
+      <div class="box_local_obras"><strong>Local: </strong>Itatiba/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Irmãos Russi Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Edifício Residencial</div>
+      <div class="box_local_obras"><strong>Local: </strong>Guarujá/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Lico´s Empreendimentos Imobiliários Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Prédio Residencial</div>
+      <div class="box_local_obras"><strong>Local: </strong>Guarulhos/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Pavão Indústria e Comércio Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Ed. Residencial Argentio</div>
+      <div class="box_local_obras"><strong>Local: </strong>Campinas/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Bromberg Construtora Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Ed. Residencial Melina</div>
+      <div class="box_local_obras"><strong>Local: </strong>Valinhos/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Mogmo Construtora e Incorporadora Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Shopping Via Vale Garden</div>
+      <div class="box_local_obras"><strong>Local: </strong>Taubaté/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Pinese Vieira Engenharia
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Premiere M. Cond. Clube</div>
+      <div class="box_local_obras"><strong>Local: </strong>Paulínia/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Construtora Queiroz Galvão
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">S. Pague Menos</div>
+      <div class="box_local_obras"><strong>Local: </strong>Hortolândia/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>BCS Construtora e Incorporadora
+      </div>
+    </div>
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Master Business Park</div>
+      <div class="box_local_obras"><strong>Local: </strong>Jundiaí/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Enac Empreendimentos, construções e Comércio Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">SENAI Bragança Paulista</div>
+      <div class="box_local_obras"><strong>Local: </strong>Bragança Paulista/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Construtora e Incorporadora Guarany
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Americana Office Tower</div>
+      <div class="box_local_obras"><strong>Local: </strong>Americana/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Construcamp Obras e Projeto Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">GR2-Cond. Industrial</div>
+      <div class="box_local_obras"><strong>Local: </strong>Campinas/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>INI 2 Implantações Imobiliárias
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Edifício Veneza</div>
+      <div class="box_local_obras"><strong>Local: </strong>Campinas/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>MHN Construções e Comércio Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Ed. Empresarial O. Cruz</div>
+      <div class="box_local_obras"><strong>Local: </strong>Campinas/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Possato Construções e Comércio Ltda
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Germek</div>
+      <div class="box_local_obras"><strong>Local: </strong>São João da Boa Vista/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>MAZZETO E BARELA ARQUITETURA
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Hotel Intercity</div>
+      <div class="box_local_obras"><strong>Local: </strong>Vinhedo/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>SAÚVAS INCORPORADORA E ADMINISTRADORA
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Shopping</div>
+      <div class="box_local_obras"><strong>Local: </strong>Jaguariúna/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>METROCON CONSTRUTORA LTDA
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Galpão Industrial</div>
+      <div class="box_local_obras"><strong>Local: </strong>Paulínia/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>BRASWELL PAPEL E CELULOSE
+      </div>
+    </div>
+    
+    <div class="box_obras">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Residencial Aquarius</div>
+      <div class="box_local_obras"><strong>Local: </strong>São José dos Campos/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>HS TRESSOLDI INCORPORAÇÕES
+      </div>
+    </div>
+    
+    <div class="box_obras margin-0">
+    	<img src="images/megageo-obras.jpg" width="101" height="92" alt="" title="" />
+      <div class="box_title_obras">Atmosphera</div>
+      <div class="box_local_obras"><strong>Local: </strong>Jundiaí/SP</div>
+      <div class="box_local_obras caracteristicas_obras">
+      	<strong>Cliente: </strong>Queiroz Galvão Desenvolvimento Imobiliário
       </div>
     </div>
 </div>
